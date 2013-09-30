b367e8bbad6084e82ed4062e13267f45d6b81bbb
rpotucek <rpotucek@rpimac.smarttech.inc>
2013-09-30 13:54:40 -0600

    Added a slow-down timer so delete.do calls only happen every 5s. Added a window close timer to dispose of the newly opened windows after 10s.
    The timer is a bad design but because delete.do returns a 302 on success there
    is no way to inject a "close-on-done" script. It *would* be possible to set up
    a wait loop to check each owned window to see if it has already returned.

--- forceok.js
+++ forceok.js
@@ -5,12 +5,18 @@
   getargs=window.location.search.substr(1).split("&");
   for (ii=0; ii<getargs.length; ii++) {
     if (getargs[ii]=="rp-multi-delete=1") {
+
+      // Change the name of the submit button so we know the script
+      // executed and clicked the button
       button = document.getElementsByClassName('goog-button')[0];
       button.value="Confirming";
       button.click();
       button.value="Autoconfirmed by rp-multi-delete";
-      // if we do this immediately then the action isn't completed ... so skip this and get user to clean up
-      // window.close();
+
+      // Once we cliced the button we will either get an error or a 302 to
+      // the version page. Either way we want to close the window. Do so
+      // after a sensible timeout
+      setTimeout(function() { window.close(); },5000);
     }
   }
 
--- inject_js_1.js
+++ inject_js_1.js
@@ -1,6 +1,9 @@
+var wins=new Array;
+
 function checkMultiDelete() {
   var checkboxes=document.getElementsByName("rp-multi-delete");
   var dialog_string="";
+  var dialog_count=0;
   var urls=new Array;
   var ii;
   var jj;
@@ -8,6 +11,7 @@ function checkMultiDelete() {
   var app_parms;
   var app_id;
   var app_version;
+  var win;
 
   for (ii=0; ii<checkboxes.length; ii++) {
     if (checkboxes[ii].checked) {
@@ -25,20 +29,43 @@ function checkMultiDelete() {
         if (tt[0] == "delete_version") { app_version=tt[1]; }
       }
       if ((app_version != "") && (app_id != "")) {
+        dialog_count++;
         dialog_string += app_version + "\n";
         urls.push("https://appengine.google.com/deployment/deleteversion?app_id=" + app_id + "&delete_version=" + app_version + "&rp-multi-delete=1");
         console.log(ii + " - " + urls);
       }
     }
   }
-  if (confirm("Really delete these versions?\n" + dialog_string)) {
-    for (ii=0; ii<urls.length; ii++) {
-      console.log("Should open window for " + urls[ii]);
-      // setTimeout(function(){window.open(urls[ii],"_blank" + ii);});
-      win=window.open(urls[ii], "_blank_" + ii);
-      // win.focus();
-      this.focus();
+  if (confirm("Really delete these " + dialog_count + " versions?\n" + dialog_string)) {
+
+
+    function openAndDelete(index,url) {
+      var odwin;
+      console.log("Should open window (" + index + ") for " + url);
+      odwin=window.open(url, "_blank_" + index);
+      console.log('... window: ' + odwin);
+      setTimeout(function() {
+          console.log("trying to close window (" + index + ") for " + url);
+          console.log(odwin);
+          odwin.close(); 
+        },10000);
     }
+
+    function deleteLoop() {
+      console.log(ii + " < " + urls.lenght);
+      if (ii < urls.length) {
+        openAndDelete(ii,urls[ii]);  
+        setTimeout(function() { deleteLoop(); },5000);
+        ii++;
+      } else {
+        console.log("Done");
+      }
+      win.focus();
+    }
+
+    ii=0;
+    win=this;
+    deleteLoop();
     // chrome.tabs.create({ url: urls });
     //chrome.tabs.create({ url: "http://google.com" });
   }
